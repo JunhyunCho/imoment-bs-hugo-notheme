a01de97e71dc86b072417e216567afa48194a1e3
JunhyunCho <junhyuncho@gmail.com>
2024-10-22 14:38:43 +0900

    image 추가

--- imoment-lacuna-website/src/views/Scene1.vue
+++ imoment-lacuna-website/src/views/Scene1.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="scene1-container">
     <h1 class="scene-title">Scene 1</h1>
+    <img src="/images/train.png" alt="Train" class="train-image" />
     <div class="content">
       <p>여기에 Scene 1의 내용을 넣으세요.</p>
     </div>
@@ -21,7 +22,7 @@ export default {
   methods: {
     playAudio() {
       const audioPlayer = this.$refs.audioPlayer;
-      audioPlayer.volume = 0.9; // 볼륨을 90%로 설정
+      audioPlayer.volume = 0.8; // 볼륨을 50%로 설정
       audioPlayer.play().catch(() => {
         console.log("Auto-play was prevented. Please enable auto-play in your browser settings.");
       });
@@ -42,6 +43,13 @@ export default {
 .scene-title {
   font-size: 24px;
   margin-bottom: 20px;
+}
+
+.train-image {
+  width: 70%; /* 가로 기준으로 70% 크기 */
+  max-width: 600px; /* 최대 너비를 설정하여 너무 커지지 않도록 함 */
+  height: auto; /* 비율을 유지하기 위해 자동으로 높이 조정 */
+  margin-bottom: 20px; /* 이미지와 다른 요소 간의 간격 */
 }
 
 .content {
