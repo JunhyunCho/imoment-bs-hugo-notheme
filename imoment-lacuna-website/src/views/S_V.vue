509d01c765660e1ce49176c5b5e83e71d60e89ba
JunhyunCho <junhyuncho@gmail.com>
2024-10-30 14:25:58 +0900

    S_V에도 스킵버튼 추가

--- imoment-lacuna-website/src/views/S_V.vue
+++ imoment-lacuna-website/src/views/S_V.vue
@@ -1,5 +1,7 @@
 <template>
     <div class="video-container">
+        <button class="skip-button" @click="handleSkip"></button>
+
         <video 
             ref="videoPlayer"
             class="video-player"
@@ -47,6 +49,13 @@ export default {
                     this.handleVideoEnd();
                 });
             }
+        },
+        handleSkip() {
+            const video = this.$refs.videoPlayer;
+            if (video) {
+                video.pause();
+            }
+            this.handleVideoEnd();
         }
     }
 }
@@ -90,4 +99,21 @@ export default {
     background-color: #f0f0f0;
     border-color: #333;
 }
+
+.skip-button {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 20vh;
+    background: transparent;
+    border: none;
+    cursor: pointer;
+    z-index: 100;
+}
+
+/* 디버깅용 스타일 (필요시 주석 해제)
+.skip-button:hover {
+    background: rgba(255, 255, 255, 0.1);
+} */
 </style>
