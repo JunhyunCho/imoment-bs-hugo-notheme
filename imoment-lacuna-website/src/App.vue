dbf42aa008e677c4d88f6f2c99924336e479e740
JunhyunCho <junhyuncho@gmail.com>
2024-10-30 14:53:40 +0900

    S0 삭제.

--- imoment-lacuna-website/src/App.vue
+++ imoment-lacuna-website/src/App.vue
@@ -4,7 +4,6 @@
         <audio ref="testBGM" src="/lacuna/audio/bgm.mp3" preload="auto"></audio>
 
         <audio ref="all" src="/lacuna/audio/all_new.mp3" preload="auto"></audio>
-        <audio ref="S0" src="/lacuna/audio/S0.mp3" preload="auto"></audio>
         <audio ref="S1" src="/lacuna/audio/S1.mp3" preload="auto"></audio>
         <audio ref="S2" src="/lacuna/audio/S2.mp3" preload="auto"></audio>
 
