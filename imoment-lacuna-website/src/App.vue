d1c43a6095a8ac0a7102d5f64e61eb8b959491be
JunhyunCho <junhyuncho@gmail.com>
2024-10-30 12:26:39 +0900

    all.mp3 -> aac로 수정, 타이밍 수정

--- imoment-lacuna-website/src/App.vue
+++ imoment-lacuna-website/src/App.vue
@@ -8,7 +8,7 @@
 
         <audio ref="mailFromK" src="/lacuna/audio/mailFromK.mp3" preload="auto"></audio>
 
-        <audio ref="all" src="/lacuna/audio/all.mp3" preload="auto"></audio>
+        <audio ref="all" src="/lacuna/audio/all.m4a" preload="auto"></audio>
         <audio ref="S0" src="/lacuna/audio/S0.mp3" preload="auto"></audio>
         <audio ref="S1" src="/lacuna/audio/S1.mp3" preload="auto"></audio>
         <audio ref="S2" src="/lacuna/audio/S2.mp3" preload="auto"></audio>
